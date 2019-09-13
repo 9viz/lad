6481b69ee6f6d0e59427618af501df441c47fe62
viz <visuweshmuthukumar@gmail.com>
2019-09-13 19:17:34 +0530

    lad: clean up functions
    handle-lists is replaced by handle-nums which handles numbers instead of lists.
    just like its predecessor, it does the same thing except it converts the numbers to
    list of digits itself instead.
    
    extract-digit is a bit cleaner because of skipping one set! call
    
    ladd is simple now because handle-nums does a bit more

--- lad.rkt
+++ lad.rkt
@@ -5,10 +5,10 @@
 (define (prepend-zeros num-zeros lst)
   (append (make-list num-zeros 0) lst))
 
-(define (handle-lists lst1 lst2)
-  (let ([n1 (length lst1)]
-        [n2 (length lst2)])
-    (let ([diff (- n1 n2)])
+(define (handle-nums num1 num2)
+  (let ([lst1 (extract-digits num1)]
+        [lst2 (extract-digits num2)])
+    (let ([diff (- (length lst1) (length lst2))])
       (cond
         [(positive? diff) (values lst1 (prepend-zeros diff lst2))]
         [else (values (prepend-zeros (- diff) lst1) lst2)]))))
@@ -22,17 +22,15 @@
 
 (define extract-digits
   (lambda (num [digits '()])
-  (cond
-    [(= num 0) (reverse digits)]
-    [else
-     (set! digits (append digits
-                          [list (modulo num 10)]))
-     (extract-digits (quotient num 10)
-                     digits)])))
+    (cond
+      [(= num 0) (reverse digits)]
+      [else
+       (extract-digits (quotient num 10)
+                       (append digits [list (modulo num 10)]))])))
 
 (define (ladd n1 n2)
-  (let ([n1s (extract-digits n1)]
-        [n2s (extract-digits n2)])
-    (set!-values (n1s n2s) (handle-lists n1s n2s))
-    (list->num (map (lambda (num1 num2) (max num1 num2))
-                    n1s n2s))))
+  (let-values ([(d1 d2) (handle-nums n1 n2)])
+    (list->num
+    (map (lambda (num1 num2)
+            (max num1 num2))
+            d1 d2))))
