e17664457a2cdc1cf758de984175f829e4dd93bb
viz <visuweshmuthukumar@gmail.com>
2019-09-13 18:31:55 +0530

    lad: turn pzr lambda to a function

--- lad.rkt
+++ lad.rkt
@@ -2,6 +2,9 @@
 
 #lang racket
 
+(define (prepend-zeros num-zeros lst)
+  (append (make-list num-zeros 0) lst))
+
 (define extract-digits
   (lambda (num [digits '()])
   (cond
@@ -15,14 +18,12 @@
 (define (ladd n1 n2)
   (let ([n1s (extract-digits n1)]
         [n2s (extract-digits n2)]
-        [pzr (lambda (diff lst)
-               (append (make-list diff 0) lst))])
     (let ([ln1 (length n1s)]
           [ln2 (length n2s)])
       (cond
         [(> ln1 ln2) (set! n2s
-                            (pzr (- ln1 ln2) n2s))]
+                            (prepend-zeros (- ln1 ln2) n2s))]
         [(< ln1 ln2) (set! n1s
-                           (pzr (- ln2 ln1) n1s))])
+                           (prepend-zeros (- ln2 ln1) n1s))])
       (map (lambda (num1 num2) (max num1 num2))
            n1s n2s))))
