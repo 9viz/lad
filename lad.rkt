3f23105cdd1313a3bfab34f0a0c7b163c7bf2c1d
viz <visuwesh@tutanota.com>
2020-07-01 19:44:53 +0530

    Rewrite

--- lad.rkt
+++ lad.rkt
@@ -1,13 +1,37 @@
-;; lad - lunar (dismal) arithmetic repl
-;; licensed under bsd 2 clause simplified license
+#lang racket/base
 
-#lang racket
+(define (l+ . y)
+  (define (helper a b c a0 b0)
+    (cond
+      ((zero? b) (+ (* a a0) c))
+      ((zero? a) (+ (* b b0) c))
+      (else (helper
+             (quotient a 10)
+             (quotient b 10)
+             (+ c (* b0
+                     (max (remainder a 10)
+                          (remainder b 10))))
+             (* 10 a0)
+             (* b0 10)))))
+  (foldl (λ (s t)
+           (helper s t 0 1 1))
+         (car y)
+         (cdr y)))
 
-(require "lib/parse.rkt")
-
-(let loop ()
-  (display "> ")
-  (printf "~a\n"
-          (leval (open-input-string
-                  (read-line (current-input-port) 'any))))
-  (loop))
+(define (l* . y)
+  (define (helper a b c a0 b0)
+    (cond
+      ((zero? b) (+ (* a a0) c))
+      ((zero? a) (+ (* b b0) c))
+      (else (helper
+             (quotient a 10)
+             (quotient b 10)
+             (+ c (* b0
+                     (min (remainder a 10)
+                          (remainder b 10))))
+             (* 10 a0)
+             (* b0 10)))))
+  (foldl (λ (s t)
+           (helper s t 0 1 1))
+         (car y)
+         (cdr y)))
--- lib/mult.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(provide (all-defined-out))
-
-(define (lmult/append-zeros num-digits lst)
-  (append lst (make-list num-digits 0)))
-
-(define (lmult/mult-single ns n)
-  (map (λ (d) [min d n]) ns))
